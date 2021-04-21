b961ce0ff359ae49bb472ed09fb5c9a59e83cea2
nspranav1180 <nspranav1180@gmail.com>
2021-04-21 12:26:07 +0530

    Created structure vectors -saved structure vecs as pickles -Triangulation simplices as pickles

--- generate_sequences.py
+++ generate_sequences.py
@@ -1,14 +1,23 @@
 #%%
 from random import choice
+from string import ascii_uppercase
 
 def random_string(length):
     """Produce a random string made of *length* uppercase ascii 
      characters"""
-
-    return ''.join(choice(['A','G','C','T']) for i in range(length)) 
+    amino_acids = set(ascii_uppercase).difference(set(['B','O','J','U','X','Z']))
+    return ''.join(choice(list(amino_acids)) for i in range(length)) 
 # %%
 
 def test_random_string():
     s = random_string(200)
     str = set(s)
     assert len(str) == 4
+
+# %%
+from random import choices
+def caching_fib(length):
+    l = {i:0 for i in range(length)}
+
+    return l
+# %%
